8d1562ff8bc2ed39423f52fca61c90cd148665cf
thegazzella <154521611+thegazzella@users.noreply.github.com>
2026-06-08 17:47:19 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from .trading_calendar import TradingCalendar
-from .presets import ALL_DAYS_CALENDAR, WEEKDAYS_CALENDAR, SPX_CALENDAR
-
-__all__ = [
-    "TradingCalendar",
-    "ALL_DAYS_CALENDAR",
-    "WEEKDAYS_CALENDAR",
-    "SPX_CALENDAR",
-]
